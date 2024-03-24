58122c19a3cddc644039fdb771722977c0b3c64d
CresBloomoon <metroidm2421@gmail.com>
2024-03-25 00:26:13 +0900

    変更しました

--- ボタンのスタイルをいじってみる/MainWindowViewModel.cs
+++ ボタンのスタイルをいじってみる/MainWindowViewModel.cs
@@ -6,14 +6,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using ボタンのスタイルをいじってみる.Resources.Windows;
 
 namespace ボタンのスタイルをいじってみる {
     internal class MainWindowViewModel : INotifyPropertyChanged {
 
+        private readonly Window _window;
+
         public ReactiveCommandSlim ExecuteCommand { get; } = new ReactiveCommandSlim();
         public ReactiveCommandSlim CancelCommand { get; } = new ReactiveCommandSlim();
 
-        public MainWindowViewModel() {
+        public MainWindowViewModel(Window window = null) {
+            _window = window;
             ExecuteCommand.Subscribe(_ => ExecuteButtonExecute());
             CancelCommand.Subscribe(_ => CancelButtonExecute());
         }
@@ -21,7 +25,7 @@ namespace ボタンのスタイルをいじってみる {
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void ExecuteButtonExecute() {
-            MessageBox.Show("Executed.");
+            MessageEx.ShowInformationDialog("information.", MessageBoxButton.OKCancel, _window);
         }
 
         private void CancelButtonExecute() {
